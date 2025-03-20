9c8a0b6308c18abe88215713b5d507fe112f518a
Dickson Esamai <mukweesamai@gmail.com>
2025-03-20 21:08:26 +0300

    updated hasLiked

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -5,13 +5,21 @@ import './App.css'
 
 
 const Card = ( {title}) => {
+  const [hasLiked, setHasLiked] = useState(false);
+
   return (
     <div className="card">
       <h2>{ title } </h2>
+
+      <button onClick={() => setHasLiked(true)}>
+        {hasLiked ? '❤️' : '🤍'}
+      </button>
     </div>
   )
 }
 const App = () => {
+
+
   return (
     <div className="card-container">
     <Card title="Star Wars" />

182bdcbcc20850fb9e76a598618dd41b19d97ca2
Dickson Esamai <mukweesamai@gmail.com>
2025-03-20 22:13:44 +0300

    count from 1 upwards

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
+import { use, useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 
 const Card = ( {title}) => {
+  const [count, setCount] = useState(0);
   const [hasLiked, setHasLiked] = useState(false);
 
-  return (
-    <div className="card">
-      <h2>{ title } </h2>
+  useEffect(() => {
+    console.log(`The ${title} has been liked: ${hasLiked}`);
+  }, [hasLiked, title]);
 
-      <button onClick={() => setHasLiked(true)}>
+
+  return (
+    <div className="card"  onClick={() => setCount(count + 1)}>
+      <h2>{ title } <br /> {count || null}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
         {hasLiked ? '❤️' : '🤍'}
       </button>
     </div>

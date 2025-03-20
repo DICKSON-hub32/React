a10a60b8b43469fb8438543009b8ab6da8123627
Dickson Esamai <mukweesamai@gmail.com>
2025-03-20 17:33:30 +0300

    Inline and external css linking

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -6,16 +6,14 @@ import './App.css'
 
 const Card = ( {title}) => {
   return (
-    <div>
+    <div className="card">
       <h2>{ title } </h2>
     </div>
   )
 }
 const App = () => {
   return (
-    <div>
-    <h2>Functional Arrow Component</h2>
-
+    <div className="card-container">
     <Card title="Star Wars" />
     <Card title="Avatar" />
     <Card title="The Lion King" />

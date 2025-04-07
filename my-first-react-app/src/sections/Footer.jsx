e8c4dc08014fb7c08236f0bb35fcf09317b80cfa
Dickson Esamai <mukweesamai@gmail.com>
2025-04-07 12:40:04 +0300

    imported assets folder to Footer.jsx to solve the Logo display issue in the footer

--- my-first-react-app/src/sections/Footer.jsx
+++ my-first-react-app/src/sections/Footer.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import logo from "../assets/logo.png";
 const Footer = () => {
   return (
     <footer className="mt-20 py-8 text-center text-gray-300 bg-gray-900 rounded-t-[50%_20px] relative overflow-hidden">
       <div className="absolute top-0 left-0 w-full h-1 bg-gradient-to-r from-[#D6C7FF] to-[#AB8BFF]"></div>
       <img
-        src="./src/assets/logo.png"
+        src={logo}
         alt="Teckish Logo"
         className="mx-auto w-24 h-auto mb-4 rounded-lg shadow-lg shadow-[#D6C7FF]/20 border border-[#AB8BFF]/30"
       />
